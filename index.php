9799dafc89f0fa8170c44c237796ad799f0a2537
= <34792643+iBhavesh@users.noreply.github.com>
2020-10-14 18:48:45 +0530

    Update Stucture

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
-<?php_check_syntax
-    echo "Working";
-    
-?>
+<!DOCTYPE html>
+<html lang="en">
+    <?php include('head.php'); ?>
+    <body>
+        <?php include('hotels.php'); ?>
+        <?php include('footer.php');?>
+    </body>
+</html>
