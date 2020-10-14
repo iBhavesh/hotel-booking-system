21b873e9878a836d08fd96f706d7502eeb34c299
Namrota Dutta <namrota.dutta@gmail.com>
2020-10-14 23:23:45 +0530

    add signup.php

--- head.php
+++ head.php
@@ -2,5 +2,5 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Book Hotel Online</title>
-    <link rel="stylesheet" href="./assets/style.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
 </head>
---
+++ signup.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?php include('head.php'); ?>
+    <body>
+        <!----Signup div Start-=--->    
+        <div class="signup-div">
+            <form action="" method="post">
+                <input type="text" name="fname" id="" required>
+                <input type="email" name="email" id="" required>
+            </form>
+        </div>
+
+        <!----Signup div End--->
+
+
+
+
+        <?php include('footer.php');?>
+    </body>
+</html>
