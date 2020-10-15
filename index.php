738acbb5e87b3640540b4f9a37fef6b80c6f2410
Nishanka <nishankamalakar96@gmail.com>
2020-10-15 13:08:55 +0530

    add navbar

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="en">
     <?php include('head.php'); ?>
     <body>
+        <?php include('navbar.php'); ?>
         <?php include('hotels.php'); ?>
         <?php include('footer.php');?>
     </body>
